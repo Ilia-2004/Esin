f483291a9fcf073ab128b507d22031252aff4f19
BonkersDenis <118104588+BonkersDenis@users.noreply.github.com>
2023-10-14 08:20:29 +0300

    update CI

--- esin-first-task/Program.cs
+++ esin-first-task/Program.cs
@@ -14,29 +14,20 @@ namespace esin_first_task
       
       while (true)
       { 
-        Console.WriteLine("\u001b[1mGRAPH ADJACENCY LIST\u001b[0m");
-        Console.WriteLine(" \u001b[1m$ \u001b[0m" + 
-                          "what do you want to do? | if you need help input '--h'");
-        Console.Write("  \u001b[1m> \u001b[0m"); command = Console.ReadLine();
+        Console.WriteLine("GRAPH ADJACENCY LIST");
+        Console.WriteLine(" $ what do you want to do? | if you need help input '--h'");
+        Console.Write("  > "); command = Console.ReadLine();
         
         if (command == "--h")
         {
-          Console.WriteLine(" \u001b[1m$ \u001b[0m" +
-                            "output an adjacency list,     '--out-list'");
-          Console.WriteLine(" \u001b[1m$ \u001b[0m" + 
-                            "add a graph vertex,           '--add-vert'");
-          Console.WriteLine(" \u001b[1m$ \u001b[0m" +
-                            "add a graph edge,             '--add-edge'");
-          Console.WriteLine(" \u001b[1m$ \u001b[0m" + 
-                            "remove a graph vertex,        '--rem-vert'");
-          Console.WriteLine(" \u001b[1m$ \u001b[0m" + 
-                            "add a graph egde,             '--rem-egde'");
-          Console.WriteLine(" \u001b[1m$ \u001b[0m" +
-                            "add a graph egde,             '--rem-egde'");
-          Console.WriteLine(" \u001b[1m$ \u001b[0m" +
-                            "if you need help,             '--h'");
-          Console.WriteLine(" \u001b[1m$ \u001b[0m" +
-                            "exit programm,                '--e'");
+          Console.WriteLine(" $ output an adjacency list,     '--out-list'");
+          Console.WriteLine(" $ add a graph vertex,           '--add-vert'");
+          Console.WriteLine(" $ add a graph edge,             '--add-edge'");
+          Console.WriteLine(" $ remove a graph vertex,        '--rem-vert'");
+          Console.WriteLine(" $ add a graph egde,             '--rem-egde'");
+          Console.WriteLine(" $ add a graph egde,             '--rem-egde'");
+          Console.WriteLine(" $ if you need help,             '--h'");
+          Console.WriteLine(" $ exit programm,                '--e'");
           Console.ReadKey();
           Console.Clear();
         }
@@ -51,14 +42,14 @@ namespace esin_first_task
         {
           string vertex = "";
           
-          Console.WriteLine(" \u001b[1m$ \u001b[0minput your vertex:");
-          Console.Write("  \u001b[1m> \u001b[0m"); vertex = Console.ReadLine();
+          Console.WriteLine(" $ input your vertex:");
+          Console.Write("  > "); vertex = Console.ReadLine();
           
           bool addVertex = objectGraph.AddVertex(vertex);
           if (addVertex)
-            Console.WriteLine(" \u001b[1m$ \u001b[0myour vertex has been successfully added");
+            Console.WriteLine(" $ your vertex has been successfully added");
           else 
-            Console.WriteLine(" \u001b[1m$ \u001b[0m[ERROR]: you wrote an incorrect command!");
+            Console.WriteLine(" $ [ERROR]: you wrote an incorrect command!");
           
           Console.ReadKey();
           Console.Clear();
@@ -70,20 +61,20 @@ namespace esin_first_task
           int? weight = null; 
           bool isDirected = false; 
           
-          Console.WriteLine(" \u001b[1m$ \u001b[0minput your edge 'from':");
-          Console.Write("  \u001b[1m> \u001b[0m"); from = Console.ReadLine();
-          Console.WriteLine(" \u001b[1m$ \u001b[0minput your edge 'to':");
-          Console.Write("  \u001b[1m> \u001b[0m"); to = Console.ReadLine();
-          Console.WriteLine(" \u001b[1m$ \u001b[0minput your edge weight:");
-          Console.Write("  \u001b[1m> \u001b[0m"); weight = Convert.ToInt32(Console.ReadLine());
-          Console.WriteLine(" \u001b[1m$ \u001b[0minput directed:");
-          Console.Write("  \u001b[1m> \u001b[0m"); isDirected = Convert.ToBoolean(Console.ReadLine());
+          Console.WriteLine(" $ input your edge 'from':");
+          Console.Write("  > "); from = Console.ReadLine();
+          Console.WriteLine(" $ input your edge 'to':");
+          Console.Write("  > "); to = Console.ReadLine();
+          Console.WriteLine(" $ input your edge weight:");
+          Console.Write("  > "); weight = Convert.ToInt32(Console.ReadLine());
+          Console.WriteLine(" $ input directed:");
+          Console.Write("  > "); isDirected = Convert.ToBoolean(Console.ReadLine());
           
           bool addEdge = objectGraph.AddEdge(from, to, weight, isDirected);
           if (addEdge) 
-            Console.WriteLine(" \u001b[1m$ \u001b[0myour edge has been successfully added");
+            Console.WriteLine(" $ your edge has been successfully added");
           else 
-            Console.WriteLine(" \u001b[1m$ \u001b[0m[ERROR]: you wrote an incorrect command!");
+            Console.WriteLine(" $ [ERROR]: you wrote an incorrect command!");
           
           Console.ReadKey();
           Console.Clear();
@@ -92,14 +83,14 @@ namespace esin_first_task
         {
           string vertex = "";
           
-          Console.WriteLine(" \u001b[1m$ \u001b[0minput your vertex:");
-          Console.Write("  \u001b[1m> \u001b[0m"); vertex = Console.ReadLine();
+          Console.WriteLine(" $ input your vertex:");
+          Console.Write("  > "); vertex = Console.ReadLine();
           
           bool remVert = objectGraph.RemoveVertex(vertex);
           if (remVert)
-            Console.WriteLine(" \u001b[1m$ \u001b[0myour vertex has been successfully removed");
+            Console.WriteLine(" $ your vertex has been successfully removed");
           else 
-            Console.WriteLine(" \u001b[1m$ \u001b[0m[ERROR]: you wrote an incorrect command!");
+            Console.WriteLine("$ [ERROR]: you wrote an incorrect command!");
           
           Console.ReadKey();
           Console.Clear();
@@ -111,18 +102,18 @@ namespace esin_first_task
           int? weight = null; 
           bool isDirected = false; 
           
-          Console.WriteLine(" \u001b[1m$ \u001b[0minput your vertex 'from':");
-          Console.Write("  \u001b[1m> \u001b[0m"); from = Console.ReadLine();
-          Console.WriteLine(" \u001b[1m$ \u001b[0minput your vertex 'to':");
-          Console.Write("  \u001b[1m> \u001b[0m"); to = Console.ReadLine();
-          Console.WriteLine(" \u001b[1m$ \u001b[0minput directed:");
-          Console.Write("  \u001b[1m> \u001b[0m"); isDirected = Convert.ToBoolean(Console.ReadLine());
+          Console.WriteLine("$ input your vertex 'from':");
+          Console.Write(" > "); from = Console.ReadLine();
+          Console.WriteLine("$ input your vertex 'to':");
+          Console.Write(" > "); to = Console.ReadLine();
+          Console.WriteLine("$ input directed:");
+          Console.Write(" > "); isDirected = Convert.ToBoolean(Console.ReadLine());
           
           bool remEdge = objectGraph.RemoveEdge(from, to, isDirected);
           if (remEdge)
-            Console.WriteLine(" \u001b[1m$ \u001b[0myour edge has been successfully removed");
+            Console.WriteLine("$ your edge has been successfully removed");
           else 
-            Console.WriteLine(" \u001b[1m$ \u001b[0m[ERROR]: you wrote an incorrect command!");
+            Console.WriteLine("$ [ERROR]: you wrote an incorrect command!");
           
           Console.ReadKey();
           Console.Clear();
@@ -130,7 +121,7 @@ namespace esin_first_task
         else if (command == "--e") break;
         else
         {
-          Console.WriteLine(" \u001b[1m$ \u001b[0m[ERROR]: you wrote an incorrect command!");
+          Console.WriteLine("$ [ERROR]: you wrote an incorrect command!");
           Console.ReadKey();
           Console.Clear();
         }

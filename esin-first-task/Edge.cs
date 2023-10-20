a0ee1da73189e447846eb55486bc27fabcf3e482
BonkersDenis <118104588+BonkersDenis@users.noreply.github.com>
2023-10-20 08:08:47 +0300

    Update the class Edge

--- esin-first-task/Edge.cs
+++ esin-first-task/Edge.cs
@@ -6,6 +6,6 @@ public class Edge
   public string? To { get; set; }
   public int? Weight { get; set; }
 
-  // constructors
+  // constructor
   public Edge(string? to, int? weight) => (To, Weight) = (to, weight);
 }

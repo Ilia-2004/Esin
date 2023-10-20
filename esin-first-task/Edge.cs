6ef4b9b8f1d2704b853869008b0f630a6954c0ff
BonkersDenis <118104588+BonkersDenis@users.noreply.github.com>
2023-10-20 08:15:39 +0300

    Update type of data in the class Edge

--- esin-first-task/Edge.cs
+++ esin-first-task/Edge.cs
@@ -3,9 +3,9 @@ namespace esin_first_task;
 
 public class Edge
 {
-  public string? To { get; set; }
+  public string To { get; set; }
   public int? Weight { get; set; }
 
   // constructor
-  public Edge(string? to, int? weight) => (To, Weight) = (to, weight);
+  public Edge(string to, int? weight) => (To, Weight) = (to, weight);
 }
